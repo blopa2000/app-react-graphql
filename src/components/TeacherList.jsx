44ca78f69d44315107e87446cdee3d23fa93362a
blopa2000 <jpagudelo2000@gmail.com>
2020-11-28 13:57:06 -0500

    test mutation

--- src/components/TeacherList.jsx
+++ src/components/TeacherList.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "@apollo/react-hooks";
+import { useQuery, useMutation } from "@apollo/react-hooks";
 import { gql } from "apollo-boost";
 
 const GET_TEACHERS = gql`
@@ -13,20 +13,54 @@ const GET_TEACHERS = gql`
   }
 `;
 
+const CREATE_TEACHER = gql`
+  mutation CreateTeacher(
+    $firstName: String!
+    $lastName: String!
+    $gender: Gender
+    $aptitudes: [String]
+  ) {
+    createTeacher(
+      input: { firstName: $firstName, lastName: $lastName, gender: $gender, aptitudes: $aptitudes }
+    ) {
+      _id
+      firstName
+      lastName
+      gender
+      aptitudes
+    }
+  }
+`;
+
 const TeacherList = () => {
   const { loading, error, data } = useQuery(GET_TEACHERS);
+  const [createTeacher] = useMutation(CREATE_TEACHER);
+
   if (loading) return <p>Loading...</p>;
   if (error) {
     console.log(error);
     return <p>error</p>;
   }
 
+  const handleCreateTeacher = async () => {
+    const firstName = "un nuevo dato de prueba numero 2";
+    const lastName = "el mismo 2";
+    const gender = "M";
+    const aptitudes = ["uno", "dos", "tres"];
+    const res = await createTeacher({ variables: { firstName, lastName, gender, aptitudes } });
+    console.log(res.data.createTeacher);
+  };
+
   return (
     <div>
       <h1>Teachers</h1>
-      {data.getTeachers.map((teacher) => (
-        <p>{teacher._id}</p>
+      {data.getTeachers.map((teacher, id) => (
+        <p key={id}>{teacher._id}</p>
       ))}
+
+      <button className="btn btn-primary" onClick={handleCreateTeacher}>
+        click
+      </button>
     </div>
   );
 };
